d2ace174dd6ace55ad81f18c43902ca35e5e8162
masayoshikob <masayoshikob@gmail.com>
2023-08-14 18:44:32 +1200

    prep 0.0.1b

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -12,62 +12,72 @@ set step=cls
 goto BuildScript
 
 :PauseExit
-pause>null
-del /f /q null
-cls
-exit
+    pause>null
+    del /f /q null
+    cls
+    exit
 
 :BuildScript
 :: Step 1
-%step%
-echo [#                   ] 5%
-echo.
-echo Cleaning old build files...
-:: Deletes any old build folders present.
-rmdir /q /s ..\build\win
+    %step%
+    echo [#                   ] 5/100
+    echo.
+    echo Cleaning old build files...
+    :: Deletes any old build folders present.
+        rmdir /q /s ..\build\win
 
 :: Step 2
-%step%
-echo [##                  ] 10%
-echo.
-echo Creating build folder...
-:: Creates the destination folder for the build.
-mkdir ..\build\win
+    %step%
+    echo [##                  ] 10/100
+    echo.
+    echo Creating build folder...
+    :: Creates the destination folder for the build.
+        mkdir ..\build\win
 
 :: Step 3
-%step%
-echo [####                ] 20%
-echo.
-echo Windows Graphic Interface...
-:: Copies the GUI files from the source into the build folder.
-copy ..\gui\win\BitnetNode.exe ..\build\win\BitnetNode.exe
-copy ..\gui\win\version.ini ..\build\win\version.ini
+    %step%
+    echo [####                ] 20/100
+    echo.
+    echo Windows Graphic Interface...
+    :: Copies the GUI files from the source into the build folder.
+        copy ..\gui\win\BitnetNode.exe ..\build\win\BitnetNode.exe
+        copy ..\gui\win\version.ini ..\build\win\version.ini
 
 :: Step 4
-%step%
-echo [######              ] 30%
-echo.
-echo Copying binaries...
-:: Copies the entire /bin folder into the build folder.
-xcopy /e /h /c /i ..\bin ..\build\win\bin
-:: Removes unused /bin files used by other OSs for size optimisation.
-del /f /q ..\build\win\bin\bitnet
-del /f /q ..\build\win\bin\readme.md
+    %step%
+    echo [######              ] 30/100
+    echo.
+    echo Copying binaries...
+    :: Copies the entire /bin folder into the build folder.
+        xcopy /e /h /c /i ..\bin ..\build\win\bin
+    :: Removes unused /bin files used by other OSs for size optimisation.
+        del /f /q ..\build\win\bin\bitnet
+        del /f /q ..\build\win\bin\miner
+        del /f /q ..\build\win\bin\readme.md
 
 :: Step 5
-%step%
-echo [##############      ] 70%
-echo.
-echo Copying shell files...
-:: Copies the entire /shell folder into the build folder.
-xcopy /e /h /c /i ..\shell ..\build\win\shell
-:: Removes unused /shell files used by other OSs for size optimisation.
-del /f /q ..\build\win\shell\readme.md
+    %step%
+    echo [##########          ] 50/100
+    echo.
+    Copying settings...
+    :: Copies the entire /settings folder into the build folder.
+        xcopy /e /h /c /i ..\settings ..\build\win\settings
 
 :: Step 6
-%step%
-echo [####################] 100%
-echo.
-echo Process complete! You can find your optimised build inside the
-echo /build/win folder. Press any key to exit the script.
-goto PauseExit
+    %step%
+    echo [##############      ] 70/100
+    echo.
+    echo Copying shell files...
+    :: Copies the entire /shell folder into the build folder.
+        xcopy /e /h /c /i ..\shell\win ..\build\win\shell
+    :: Removes unused /shell files used by other OSs for size optimisation.
+        del /f /q ..\build\win\shell\readme.md
+
+:: Step 6
+    %step%
+    echo [####################] 100/100
+    echo.
+    echo Process complete! You can find your optimised build inside the
+    echo /build/win folder. Press any key to exit the script.
+    start ..\build\win\
+    goto PauseExit
---
+++ scripts/build.sh
@@ -0,0 +1,1 @@
+# Linux and MacOS build script.
