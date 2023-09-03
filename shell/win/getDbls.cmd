d060d4f5483b265f7af846b6d1afcd888f777a1d
masayoshikob <masayoshikob@gmail.com>
2023-09-03 14:43:44 +1200

    push update

--- shell/win/getDbls.cmd
+++ shell/win/getDbls.cmd
@@ -14,12 +14,17 @@
 :: other dealings in the software.
 
 @echo off
-title Fetching Bootnodes List...
+title Importing from DBLS...
 
 :ImportUrl
 :: imports the server url and file name from the settings repository
 set /p dblsUrl=<..\settings\bootnodes_dbls.server.set
 set /p dblsFile=<..\settings\bootnodes_dbls.file.set
+
+:Info
+echo Importing bootnodes list from DBLS. Please do not close this prompt
+echo otherwise the import might fail.
+echo ---------------------------------------------------------------------
 
 :DeleteCache
 :: deletes old cache
@@ -29,10 +34,9 @@ mkdir ..\temp
 :UseGetUrl
 :: uses getUrl to fetch from server
 .\getUrl -P ..\temp https://%dblsUrl%/%dblsFile%
+
 :: applies the default file name logic
 :: needs to loop with a sha256 name to avoid conflicts if the file
 :: downloaded is already named default.list
 ren ..\temp\%dblsFile% 37a8eec1ce19687d132fe29051dca629d164e2c4958ba141d5f4133a33f0688f
 ren ..\temp\37a8eec1ce19687d132fe29051dca629d164e2c4958ba141d5f4133a33f0688f default.list
-
-pause
--- shell/win/startNode.cmd
+++ shell/win/startNode.cmd
@@ -16,6 +16,14 @@
 @echo off
 title Bitnet Local Node
 
+:: debug settings and logic
+set debug=off
+if /i %debug% == on (
+    @echo on
+) else (
+    @echo off
+)
+
 :ImportsAndConstructors
     
     :: basic settings
@@ -28,7 +36,7 @@ title Bitnet Local Node
         if /i %ipcDisable% == true (
             set "basicFlag=--syncmode %syncMode% --maxpeers %maxPeers% --ipcdisable"
                 ) else (
-                    set "basicFlag=--syncmode %syncMode% --maxpeers %maxPeers%"
+                    set "basicFlag=--syncmode %syncMode% --maxpeers %maxPeers% --ipcpath bitnet"
                     )
     
     :: networking
@@ -56,7 +64,7 @@ title Bitnet Local Node
     
     :: http flag
     set /p httpStatus=<..\settings\node_http.set
-    set /p httpAddr=<..\settings\node_http_addr.set
+    set /p httpAddr=<..\settings\node_http.addr.set
     set /p httpCorsDomain=<..\settings\node_http.corsdomain.set
     set /p httpPort=<..\settings\node_http.port.set
     set /p httpRpcPrefix=<..\settings\node_http.rpcprefix.set
@@ -76,35 +84,38 @@ title Bitnet Local Node
                 set "httpApiP1="
                 )
         if /i %httpApiDebug% == true (
-            set "httpApiP2=debug,%httpApiP1%"
+            set "httpApiP2=%httpApiP1%,debug"
             ) else (
                 set "httpApiP2=%httpApiP1%"
                 )
         if /i %httpApiEth% == true (
-            set "httpApiP3=eth,%httpApiP2%"
+            set "httpApiP3=%httpApiP2%,eth"
             ) else (
                 set "httpApiP3=%httpApiP2%"
                 )
         if /i %httpApiMiner% == true (
-            set "httpApiP4=miner,%httpApiP3%"
+            set "httpApiP4=%httpApiP3%,miner"
             ) else (
                 set "httpApiP4=%httpApiP3%"
                 )
         if /i %httpApiNet% == true (
-            set "httpApiP5=net,%httpApiP4%"
+            set "httpApiP5=%httpApiP4%,net"
             ) else (
                 set "httpApiP5=%httpApiP4"
                 )
         if /i %httpApiTxPool% == true (
-            set "httpApiP6=txpool,%httpApiP5%"
+            set "httpApiP6=%httpApiP5%,txpool"
             ) else (
-                set "httpApiP6=%httpApiP5"
+                set "httpApiP6=%httpApiP5%"
                 )
         if /i %httpApiWeb3% == true (
-            set "httpApiP7=web3,%httpApiP6%"
+            set "httpApiP7=%httpApiP6%,web3"
             ) else (
                 set "httpApiP7=%httpApiP6%"
                 )
+        if "%httpApiP7:~0,1%" == "," (
+            set "httpApiP7=%httpApiP7:~1%"
+        )
         set httpApi=%httpApiP7%
 
         :: flag constructor
@@ -114,15 +125,128 @@ title Bitnet Local Node
             set "httpFlag="
         )
     
+    :: graphql flag
+    set /p graphqlStatus=<..\settings\node_graphql.set
+    set /p graphqlCorsDomain=<..\settings\node_graphql.corsdomain.set
+    set /p graphqlVhosts=<..\settings\node_graphql.vhosts.set
+
+        :: flag constructor
+        if /i %graphqlStatus% == true (
+            set "graphqlFlag=--graphql --graphql.corsdomain %graphqlCorsDomain% --graphql.vhosts %graphqlVhosts%"
+        ) else (
+            set "graphqlFlag="
+        )
+
+    :: websocket flag
+    set /p wsStatus=<..\settings\node_ws.set
+    set /p wsAddr=<..\settings\node_ws.addr.set
+    set /p wsOrigins=<..\settings\node_ws.origins.set
+    set /p wsPort=<..\settings\node_ws.port.set
+    set /p wsRpcPrefix=<..\settings\node_ws.rpcprefix.set
+    set /p wsApiAdmin=<..\settings\node_ws.api.admin.set
+    set /p wsApiDebug=<..\settings\node_ws.api.debug.set
+    set /p wsApiEth=<..\settings\node_ws.api.eth.set
+    set /p wsApiMiner=<..\settings\node_ws.api.miner.set
+    set /p wsApiNet=<..\settings\node_ws.api.net.set
+    set /p wsApiTxPool=<..\settings\node_ws.api.txpool.set
+    set /p wsApiWeb3=<..\settings\node_ws.api.web3.set
+
+        :: api constructor
+        if /i %wsApiAdmin% == true (
+            set "wsApiP1=admin"
+            ) else (
+                set "wsApiP1="
+                )
+        if /i %wsApiDebug% == true (
+            set "wsApiP2=%wsApiP1%,debug"
+            ) else (
+                set "wsApiP2=%wsApiP1%"
+                )
+        if /i %wsApiEth% == true (
+            set "wsApiP3=%wsApiP2%,eth"
+            ) else (
+                set "wsApiP3=%wsApiP2%"
+                )
+        if /i %wsApiMiner% == true (
+            set "wsApiP4=%wsApiP3%,miner"
+            ) else (
+                set "wsApiP4=%wsApiP3%"
+                )
+        if /i %wsApiNet% == true (
+            set "wsApiP5=%wsApiP4%,net"
+            ) else (
+                set "wsApiP5=%wsApiP4"
+                )
+        if /i %wsApiTxPool% == true (
+            set "wsApiP6=%wsApiP5%,txpool"
+            ) else (
+                set "wsApiP6=%wsApiP5%"
+                )
+        if /i %wsApiWeb3% == true (
+            set "wsApiP7=%wsApiP6%,web3"
+            ) else (
+                set "wsApiP7=%wsApiP6%"
+                )
+        if "%wsApiP7:~0,1%" == "," (
+            set "wsApiP7=%wsApiP7:~1%"
+        )
+        set wsApi=%wsApiP7%
+
+        :: flag constructor
+        if /i %wsStatus% == true (
+            set "wsFlag=--ws --ws.port %wsPort% --ws.addr %wsAddr% --ws.origins %wsOrigins% --ws.vhosts %wsVhosts% --ws.rpcprefix %wsRpcPrefix% --ws.api %wsApi%"
+        )
+
+    :: authRpc flag
+    set /p authRpcStatus=<..\settings\node_authrpc.set
+    set /p authRpcAddr=<..\settings\node_authrpc.addr.set
+    set /p authRpcJwtSecret=<..\settings\node_authrpc.jwtsecret.set
+    set /p authRpcPort=<..\settings\node_authrpc.port.set
+    set /p authRpcVhosts=<..\settings\node_authrpc.vhosts.set
+
+        :: flag constructor
+        if /i %authRpcStatus% == true (
+            set "authRpcFlag=--authrpc.port %authRpcPort% --authrpc.addr %authRpcAddr% --authrpc.vhosts %authRpcVhosts% --authrpc.jwtsecret %authRpcJwtSecret%"
+        ) else (
+            set "authRpcFlag="
+        )
+
     :: extra flags
     set /p extraFlags=<..\settings\node_extra.set
 
-    echo %basicFlag% %networkFlag% %httpFlag% %minerFlag%
-    pause
+    :: global flag constructor
+    set "globalFlags=%basicFlag% %networkFlag% %minerFlag% %httpFlag% %graphqlFlag% %authRpcFlag% %extraFlags%"
+
+    :: debug report
+    if %debug% == on (
+        echo.
+        echo SCRIPT DEBUGGER REPORT
+        echo ---------------------------------------------------------------------------
+        echo Constructured Flags:
+        echo ---------------------------------------------------------------------------
+        echo Basic: %basicFlag%
+        echo Network: %networkFlag%
+        echo Miner: %minerFlag%
+        echo HTTP: %httpFlag%
+        echo GraphQL: %graphqlFlag%
+        echo WebSocket: %wsFlag%
+        echo AuthRPC: %authRpcFlag%
+        echo Extra: %extraFlags%
+        echo.
+        echo ---------------------------------------------------------------------------
+        echo Other Info:
+        echo ---------------------------------------------------------------------------
+        echo N/A
+        echo.
+        echo ---------------------------------------------------------------------------
+        echo The debugger has paused the script.
+        pause
+    ) else (
+        echo Starting script with debug module off...
+    )
     
 :NodeStarter
-    :: starts the node with the default plus extra flags,
-    :: and .config settings
+    :: starts the node with all the preset parameters and flags
     
     :InitGenesis
         :: initiates the genesis file
@@ -131,7 +255,7 @@ title Bitnet Local Node
         
     :StartNode
         :: starts the node
-        .\bitnet --networkid 210 --config .config %extraFlags%
+        .\bitnet --networkid 210 --config ..\settings\bootnodes.list %globalFlags%
 
 :ErrorCatcher
     :: pause to display errors and exit
